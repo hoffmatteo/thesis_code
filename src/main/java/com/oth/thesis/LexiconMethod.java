e795b8c57e7642f14ae657326d8369a2571cd348
Matteo Hoffmann <hoffmatteo@gmail.com>
2022-02-17 19:59:49 +0100

    Added intensities

--- src/main/java/com/oth/thesis/LexiconMethod.java
+++ src/main/java/com/oth/thesis/LexiconMethod.java
@@ -1,22 +1,8 @@
 package com.oth.thesis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.oth.thesis.twitter.TwitterData;
-import com.oth.thesis.twitter.TwitterResponse;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import com.oth.thesis.twitter.TwitterCrawler;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -25,18 +11,11 @@ import java.util.stream.Stream;
 public class LexiconMethod {
     private static final String sentiment_lexicon = "C:\\Users\\matte\\Desktop\\OTH\\thesis_code\\vader_lexicon.txt";
     private static final String negation_lexicon = "C:\\Users\\matte\\Desktop\\OTH\\thesis_code\\negations.txt";
-    private static Map<String, Double> sentimentDictionary = new HashMap<>();
-    private static List<String> negationDictionary = new ArrayList<>();
+    private static final String intensity_lexicon = "C:\\Users\\matte\\Desktop\\OTH\\thesis_code\\intensities.txt";
+    private static final Map<String, Double> sentimentDictionary = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    private static final List<String> negationDictionary = new ArrayList<>();
+    private static final Map<String, Double> intensityDictionary = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
 
     //TODO negation has not --> doesnt make sense
     //TODO negation no is also sentiment word?
@@ -45,87 +24,64 @@ public class LexiconMethod {
     public static void main(String[] args) {
         try {
             createSentimentDictionary();
-            createNegationDictionary();
-            //System.out.println(Arrays.toString(negationDictionary.stream().toArray()));
-            startCrawling(10);
-
-
+            createNegationList();
+            createIntensityList();
+            TwitterCrawler crawler = new TwitterCrawler();
+            crawler.crawlTweets(100, "ukraine");
+            //analyse("very not good");
         } catch (IOException ex) {
             ex.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+
         }
     }
 
     public static void analyse(String tweet) {
-        System.out.println(ANSI_GREEN + "Analyzing tweet: " + tweet + ANSI_RESET);
         String[] words = tweet.split(" ");
-        Double score = 0.0;
+        double score = 0.0;
         int index = 0;
+        //TODO better
+        for (int i = 0; i < words.length; i++) {
+            words[i] = preprocess(words[i]);
+        }
         for (String word : words) {
             if (sentimentDictionary.containsKey(word)) {
+                if (word.equals("no")) {
+                    if (words.length > index && sentimentDictionary.containsKey(words[index + 1])) {
+                        //do not add score as it is a negation
+                        index++;
+                        continue;
+                    }
+                }
                 int polarity = 1;
-                System.out.println("Detected sentiment word " + word + " with polarity " + sentimentDictionary.get(word));
-                for (int negationIndex = index - 1; negationIndex >= 0 && index - 2 <= negationIndex; negationIndex--) {
-                    if (negationDictionary.contains(words[negationIndex].toLowerCase(Locale.ROOT))) {
+                for (int tempIndex = index - 1; tempIndex >= 0 && index - 2 <= tempIndex; tempIndex--) {
+                    if (negationDictionary.contains(words[tempIndex])) {
                         polarity *= -1;
-                        //System.out.println("Detected negation word " + words[negationIndex]);
+                        //System.out.println("Detected negation word " + words[tempIndex]);
+                    } else if (intensityDictionary.containsKey(words[tempIndex])) {
+                        polarity *= intensityDictionary.get(words[tempIndex]);
                     }
                 }
                 score += polarity * sentimentDictionary.get(word);
-                System.out.println("Adding to score: " + polarity * sentimentDictionary.get(word));
+                System.out.println("Detected sentiment word " + word + " with polarity " + polarity * sentimentDictionary.get(word));
             }
             index++;
         }
-        System.out.println(score);
+        //TDOo
+        System.out.println(TwitterCrawler.ANSI_BLUE + "Final score: " + score + TwitterCrawler.ANSI_RESET);
     }
 
-    public static void startCrawling(int numTweets) throws URISyntaxException, IOException {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String bearerToken = System.getenv("bearertoken");
-            System.out.println(bearerToken);
-
-            CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(RequestConfig.custom()
-                            .setCookieSpec(CookieSpecs.STANDARD).build())
-                    .build();
-
-            URIBuilder uriBuilder = new URIBuilder("https://api.twitter.com/2/tweets/search/recent");
-
-            ArrayList<NameValuePair> queryParameters;
-            queryParameters = new ArrayList<>();
-            queryParameters.add(new BasicNameValuePair("query", "nba lang:en"));
-            queryParameters.add(new BasicNameValuePair("max_results", Integer.toString(numTweets)));
-
-            uriBuilder.addParameters(queryParameters);
-
-
-            HttpGet httpGet = new HttpGet(uriBuilder.build());
-            httpGet.setHeader("Authorization", String.format("Bearer %s", bearerToken));
-            httpGet.setHeader("Content-Type", "application/json");
-
-            CloseableHttpResponse response = httpClient.execute(httpGet);
-            HttpEntity entity = response.getEntity();
-
-            if (null != entity) {
-                String searchResponse = EntityUtils.toString(entity, "UTF-8");
-                System.out.println(searchResponse);
-                TwitterResponse myObject = objectMapper.readValue(searchResponse, TwitterResponse.class);
-                for (TwitterData data : myObject.data) {
-                    analyse(data.text);
-                }
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    //TODO emojis
+    private static String preprocess(String word) {
+        String removedPunctuation = word.replaceAll("\\p{Punct}", "");
+        return removedPunctuation.toLowerCase(Locale.ROOT);
     }
+
 
     private static void createSentimentDictionary() throws IOException {
         try (Stream<String> stream = Files.lines(Paths.get(sentiment_lexicon))) {
             stream.forEach(line -> {
                 String[] components = line.split("\t");
+
                 if (components.length >= 2) {
                     double score = Double.parseDouble(components[1]);
                     sentimentDictionary.put(components[0], score);
@@ -134,10 +90,22 @@ public class LexiconMethod {
         }
     }
 
-    private static void createNegationDictionary() throws IOException {
+    private static void createNegationList() throws IOException {
         try (Stream<String> stream = Files.lines(Paths.get(negation_lexicon))) {
             stream.forEach(line -> {
                 negationDictionary.add(line.toLowerCase(Locale.ROOT));
+            });
+        }
+    }
+
+    private static void createIntensityList() throws IOException {
+        try (Stream<String> stream = Files.lines(Paths.get(intensity_lexicon))) {
+            stream.forEach(line -> {
+                String[] components = line.split(",");
+                if (components.length == 2) {
+                    double score = Double.parseDouble(components[1]);
+                    intensityDictionary.put(components[0], score);
+                }
             });
         }
     }
---
+++ src/main/java/com/oth/thesis/twitter/TwitterCrawler.java
@@ -0,0 +1,103 @@
+package com.oth.thesis.twitter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oth.thesis.LexiconMethod;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwitterCrawler {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final int maxTweetBatch = 100; //set by twitter
+    public static final int maxNumTweets = 1000; //set by me
+    private final String bearerToken;
+    private CloseableHttpClient httpClient;
+    private final String englishTweets = " lang:en";
+    private final String noRetweets = " -is:retweet";
+
+    public TwitterCrawler() {
+        bearerToken = System.getenv("bearertoken");
+        httpClient = HttpClients.custom().setDefaultRequestConfig(RequestConfig.custom()
+                        .setCookieSpec(CookieSpecs.STANDARD).build())
+                .build();
+
+    }
+
+    public void crawlTweets(int amount, String query) {
+        if (amount > maxNumTweets) {
+            return;
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<String> results = new ArrayList<>();
+        int numTweets = 0;
+        TwitterResponse twitterReponse = null;
+
+
+        while (numTweets < amount) {
+            try {
+                URIBuilder uriBuilder = null;
+                uriBuilder = new URIBuilder("https://api.twitter.com/2/tweets/search/recent");
+
+                ArrayList<NameValuePair> queryParameters;
+                queryParameters = new ArrayList<>();
+                queryParameters.add(new BasicNameValuePair("query", query + englishTweets + noRetweets));
+                if (amount - numTweets > maxTweetBatch) {
+                    //request the maximum amount as often as needed
+                    queryParameters.add(new BasicNameValuePair("max_results", Integer.toString(maxTweetBatch)));
+                } else {
+                    queryParameters.add(new BasicNameValuePair("max_results", Integer.toString(amount - numTweets)));
+                }
+                if (numTweets != 0) {
+                    //next token ensures that no duplicate tweets are sent by twitter
+                    queryParameters.add(new BasicNameValuePair("next_token", twitterReponse.meta.next_token));
+                }
+
+                uriBuilder.addParameters(queryParameters);
+
+                HttpGet httpGet = new HttpGet(uriBuilder.build());
+                httpGet.setHeader("Authorization", String.format("Bearer %s", bearerToken));
+                httpGet.setHeader("Content-Type", "application/json");
+
+                CloseableHttpResponse response = httpClient.execute(httpGet);
+                HttpEntity entity = response.getEntity();
+
+                if (null != entity) {
+                    String searchResponse = EntityUtils.toString(entity, "UTF-8");
+                    System.out.println(searchResponse);
+                    twitterReponse = objectMapper.readValue(searchResponse, TwitterResponse.class);
+                    for (TwitterData data : twitterReponse.data) {
+                        System.out.println(ANSI_GREEN + "Found tweet: " + data.text + ANSI_RESET);
+                        LexiconMethod.analyse(data.text);
+                        numTweets++;
+                    }
+                }
+            } catch (IOException | URISyntaxException e) {
+
+            }
+        }
+
+    }
+
+
+}
